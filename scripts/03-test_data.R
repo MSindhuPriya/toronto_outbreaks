a5b6bf7e4fd0c1cad6198baf6ca3295a61f810d2
MSindhuPriya <sindhupriya.mallavarapu@mail.utoronto.ca>
2024-04-18 10:32:41 -0400

    Update references and bibliography

--- scripts/03-test_data.R
+++ scripts/03-test_data.R
@@ -1,15 +1,29 @@
 #### Preamble ####
-# Purpose: Tests... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Tests analysis data
+# Author: Sindhu Priya Mallavarapu
+# Date: 18 April, 2024
+# Contact: sindhupriya.mallavarapu@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
 
 
 #### Workspace setup ####
 library(tidyverse)
-# [...UPDATE THIS...]
 
 #### Test data ####
+test_data <- read.csv("outputs/data/analysis_data.csv")
+
+# make sure that we cleaned the data properly so that setting count is an integer
+test_data$setting_count |> class() == 'integer'
+
+# make sure we cleaned the data properly so that the type count is an integer
+test_data$type_count |> class() == 'integer'
+
+# make sure we cleaned the data properly so that the year count is an integer
+test_data$year_count |> class() == 'integer'
+
+# make sure we cleaned the data properly so that the outbreak year is an Date
+test_data$outbreak_year |> class() == 'integer'
+
+# make sure the data we're using is from 2020 to 2024
+test_data$outbreak_year |> min() >= 2020
+test_data$outbreak_year |> max() <= 2024
